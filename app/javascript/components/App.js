961f8408ced9848e29f963baeb635a406c44723d
William Segree <williamsegree@Williams-Air.lan>
2022-11-15 22:40:10 -0500

    setup complete

---
+++ app/javascript/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {
+  BrowserRouter as  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1>Todo-List!</h1>
+      </>
+    )
+  }
+}
+
+export default App
